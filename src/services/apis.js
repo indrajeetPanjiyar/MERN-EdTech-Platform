86b0c254527ad334ae0de32ad6eabc0f9ca6dd82
Indrajeet Panjiyar <indrajeetkr625@gmail.com>
2025-10-06 00:24:37 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL =  "https://mern-edtech-backend-arxt.onrender.com";
+const BASE_URL =  "https://mern-edtech-backend-arxt.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
